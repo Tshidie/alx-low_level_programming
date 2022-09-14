292e7b135fd08dcc54a911b9ed305c1599f8803b
Tshidie <tshidike@icloud.com>
2022-09-14 11:52:30 -0700

    main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,11 +2,11 @@
 #define HOLBERTON_H_INCLUDED
 
 int _putchar(char);
-void print_alphabet(void);
-void print_alphabet_x10(void);
+void print _alphabet(void);
+void print _alphabet_x10(void);
 int _islower(int c);
 int _isalpha(int c);
-int print_sign(int n);
+int print_sign(int c);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
@@ -17,4 +17,3 @@ void print_times_table(int n);
 void num(int r, int n);
 
 #endif
-		 
