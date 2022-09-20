0fd0574973922d0907d9f8814dff9c79cf3f95f6
Tshidie <tshidike@icloud.com>
2022-09-20 10:19:40 -0700

    Task 0

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 void reset_to_98(int *n);
 	void swap_int(int *a, int *b);
 	int _strlen(char *s);
@@ -9,4 +12,5 @@ void reset_to_98(int *n);
 	void print_array(int *a, int n);
 	char *_strcpy(char *dest, char *src);
 	void rev_string(char *s);
+#endif
 
